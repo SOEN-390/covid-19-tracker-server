f2df7d7100d1a9913c04a840709ffbf72363fec5
eordekian94s <eordekian94@outlook.com>
2022-01-24 19:57:32 -0500

    mysql config

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from 'express'
 const app = express()
 const PORT = 8080
 
+require('dotenv').config()
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -11,4 +13,24 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
-})
+});
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT
+});
+
+connection.connect((err: Error) => {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected to database.');
+});
+
+connection.end();
