9ca5a68cd9eb0ffb6db69416b794b8e2fd55ec03
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-01-28 20:32:59 -0500

    Create db.ts

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import {Container} from "typedi";
+
+
+export default () => {
+
+    const mysql = require('mysql');
+
+    const pool = mysql.createPool({
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+        database: 'covid-tracker'
+    });
+    Container.set('mysql', pool);
+}
