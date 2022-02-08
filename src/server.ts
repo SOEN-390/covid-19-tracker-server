df715e16c1cde38f1968f56326d21d2f0f0b0ec4
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-08 00:29:43 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -32,8 +32,7 @@ function startServer() {
                             : JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY),
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             projectId: process.env.FIREBASE_PROJECT_ID
-        }),
-        databaseURL: ''
+        })
     });
 }
 
