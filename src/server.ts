ac2e8438867d0ad91d8e766eb6e6d0ba2374cc46
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-08 09:42:33 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,16 @@ function startServer() {
 
     app.use(express.json())
 
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY[0] === '-'
+                ? process.env.FIREBASE_ADMIN_PRIVATE_KEY
+                : JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY),
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            projectId: process.env.FIREBASE_PROJECT_ID
+        })
+    });
+
     app.get('/', (req, res) => {
         res.send('Welcome to the COVID-19 Tracker App Server')
     });
@@ -24,16 +34,6 @@ function startServer() {
     app.use(config.api.prefix + config.api.version, routes());
 
     db();
-
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY[0] === '-'
-                            ? process.env.FIREBASE_ADMIN_PRIVATE_KEY
-                            : JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY),
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            projectId: process.env.FIREBASE_PROJECT_ID
-        })
-    });
 }
 
 startServer();
