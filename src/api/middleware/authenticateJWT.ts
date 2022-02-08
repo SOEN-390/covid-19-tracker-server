44669d934b708d774aa62ab83c8d9fdd62c57b9a
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-07 23:15:47 -0500

    Create authenticateJWT.ts

---
+++ src/api/middleware/authenticateJWT.ts
@@ -0,0 +1,24 @@
+import * as admin from 'firebase-admin';
+
+
+const authenticateJWT = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const idToken = authHeader.split(" ")[1];
+        admin
+            .auth()
+            .verifyIdToken(idToken)
+            .then(function (decodedToken) {
+                return next();
+            })
+            .catch(function (error) {
+                console.log(error);
+                return res.sendStatus(403);
+            });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+export default authenticateJWT;
