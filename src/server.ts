0c89c2852bcbab1b97c1517ff431cfe98964e688
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-08 10:52:53 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,8 @@ function startServer() {
         credential: admin.credential.cert({
             privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            projectId: process.env.FIREBASE_PROJECT_ID
-        })
+        }),
+        projectId: process.env.FIREBASE_PROJECT_ID
     });
 }
 
