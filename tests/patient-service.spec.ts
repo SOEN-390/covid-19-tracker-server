67210cc2f3cd2b03da450a69753fe9432433245e
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-08 00:10:11 -0500

    .

--- tests/patient-service.spec.ts
+++ tests/patient-service.spec.ts
@@ -1,10 +1,7 @@
-import resetAllMocks = jest.resetAllMocks;
+
 
 describe('Patient service unit-test', () => {
     test('HelloWorld',  () => {
         expect(true).toEqual(true);
-    })
-    afterAll(() => {
-        resetAllMocks()
     })
 });
