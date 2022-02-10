535b37bc3abc554e5ff82b1996efd864121a8899
eordekian94s <eordekian94@outlook.com>
2022-02-09 19:26:54 -0500

    insert in 3 tables when creating user

--- src/services/patient-service.ts
+++ src/services/patient-service.ts
@@ -1,26 +1,68 @@
 import {Container, Service} from "typedi";
-import {IPatientData} from "../interfaces/IPatient";
+import {IConfirmed, IPatient, IPatientData, testResult} from "../interfaces/IPatient";
+import {IUser} from "../interfaces/IUser";
 
 
 @Service()
 export default class PatientService {
 
     constructor() {
-
     }
 
-
-    async createUser(userInfo: IPatientData): Promise<boolean> {
-
+    async createUser(userInfo: IPatientData): Promise<void> {
         const db: any = Container.get('mysql');
-        const sql = `INSERT INTO User VALUES (?, ?, ?, ?, ?, ?)`
+        const sql = 'INSERT INTO User VALUES (?, ?, ?, ?, ?, ?)'
+        const user: IUser = this.getUserFromData(userInfo);
+        const patient: IPatient = this.getPatientFromData(userInfo);
+        const confirmed = userInfo.testResult === testResult.POSITIVE;
 
-        return  new Promise((resolve, reject) => {
-            db.query(sql, [userInfo.medicalId, userInfo.firstName,
-                userInfo.lastName, userInfo.phoneNumber, userInfo.address, userInfo.email], (error, result) => {
+        return new Promise((resolve, reject) => {
+            db.query(sql, [user.id, user.firstName, user.lastName, user.phoneNumber, user.address, user.email], (error, result) => {
+                if (error) {
+                    return reject(error);
+                }
+                this.createPatient(patient).then(() => {
+                    if (confirmed) {
+                        this.createConfirmed({medicalId: userInfo.medicalId, flagged: false}).then(() => {
+                        }).catch((error) => {
+                            return reject(error);
+                        })
+                    }
+                    return;
+                }).catch((error) => {
+                        return reject(error)
+                });
+            });
+        });
+    }
+
+    private async createPatient(patient: IPatient): Promise<boolean> {
+        const db: any = Container.get('mysql');
+        const sql = 'INSERT INTO Patient VALUES (?)'
+        return new Promise((resolve, reject) => {
+            db.query(sql, [patient], (error, result) => {
                 return error ? reject(error) : resolve(true);
             });
         });
+    }
 
+    private async createConfirmed(data: IConfirmed): Promise<boolean> {
+        const db: any = Container.get('mysql');
+        const sql = 'INSERT INTO Patient VALUES (?, ?)'
+        return new Promise((resolve, reject) => {
+            db.query(sql, [data], (error, result) => {
+                return error ? reject(error) : resolve(true);
+            });
+        });
+    }
+
+    private getUserFromData(userInfo: IPatientData): IUser {
+        return { id:userInfo.medicalId, firstName: userInfo.firstName,
+            lastName: userInfo.lastName, phoneNumber: userInfo.phoneNumber,
+            address: userInfo.address, email: userInfo.email}
+    }
+
+    private getPatientFromData(userInfo: IPatientData): IPatient {
+        return {medicalId: userInfo.medicalId, };
     }
 }
