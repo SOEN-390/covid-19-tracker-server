69901bad509b1b264bca8957b3c8a81a9037ea82
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-15 09:35:02 -0500

    Update user-service.spec.ts

--- tests/user-service.spec.ts
+++ tests/user-service.spec.ts
@@ -1,5 +1,93 @@
+import {testResult} from "../src/interfaces/IPatient";
+
+
 describe('User service unit-test', () => {
-    test('HelloWorld',  () => {
-        expect(true).toEqual(true);
-    })
+
+    let userId;
+    let testPatient;
+    let testDoctor;
+    let testImmigrationOfficer;
+    let testAdmin;
+    let testHealthOfficial;
+
+    beforeEach(() => {
+
+        userId = '1234';
+
+        testPatient = {
+            firstName: 'demo',
+            lastName: 'patient',
+            address: 'boul. demo',
+            email: 'demo@demo.com',
+            phoneNumber: '000-000-0000',
+            testResult: testResult.POSITIVE,
+            role: 'patient'
+        }
+
+        testDoctor = {
+            firstName: 'demo',
+            lastName: 'doctor',
+            address: 'boul. demo',
+            email: 'doctor@demo.com',
+            phoneNumber: '000-000-0000',
+            role: 'doctor'
+        }
+
+        testImmigrationOfficer = {
+            firstName: 'demo',
+            lastName: 'officer',
+            address: 'boul. demo',
+            email: 'immigration@demo.com',
+            phoneNumber: '000-000-0000',
+            role: 'immigration_officer'
+        }
+
+        testAdmin = {
+            firstName: 'demo',
+            lastName: 'admin',
+            address: 'boul. demo',
+            email: 'admin@demo.com',
+            phoneNumber: '000-000-0000',
+            role: 'admin'
+        }
+
+        testHealthOfficial = {
+            firstName: 'demo',
+            lastName: 'official',
+            address: 'boul. demo',
+            email: 'official@demo.com',
+            phoneNumber: '000-000-0000',
+            role: 'health_official'
+        }
+
+
+    });
+
+    describe('Get User with UserID (token)', () => {
+
+        test('get admin',  () => {
+
+        });
+
+        test('get immigration officer',  () => {
+
+        });
+
+        test('get health official',  () => {
+
+        });
+
+        test('get doctor',  () => {
+
+        });
+
+        test('get patient',  () => {
+
+        });
+
+    });
+
+
+
+
 });
