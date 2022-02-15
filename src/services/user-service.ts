ef6d83e647c67c19e7ea14d70c203bff22bfb9af
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-15 10:02:43 -0500

    callback to await

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -13,89 +13,91 @@ export default class UserService {
         let user = {} as IUserReturnData;
         const db: any = Container.get('mysql');
         const sql = 'SELECT * FROM User WHERE id=?';
-        return new Promise((resolve, reject) => {
-            db.query(sql, userId, async (error, result) => {
-                if (error) {
-                    return reject(error);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const [rows] = await db.query(sql, userId);
+                if (rows.length === 0) {
+                    return reject(new Error('User does not exist'));
                 }
-                if (!result[0]) {
-                    return reject(new Error('User not found'));
-                }
-                user.firstName = result[0].firstName;
-                user.lastName = result[0].lastName;
-                user.address = result[0].address;
-                user.email = result[0].email;
-                user.phoneNumber = result[0].phoneNumber;
+                user.firstName = rows[0].firstName;
+                user.lastName = rows[0].lastName;
+                user.address = rows[0].address;
+                user.email = rows[0].email;
+                user.phoneNumber = rows[0].phoneNumber;
                 try {
-                    const privilege = await this.getAuthority(userId);
-                    if (privilege) {
-                        user.role = privilege;
-                        return resolve(user);
-                    }
+                    user.role = await this.getAuthority(userId);
+                    return resolve(user);
                 } catch (e) {
                     console.log("Not an Authority");
                 }
                 try {
-                    const success = await this.getDoctor(userId);
-                    if (success) {
-                        user.role = 'doctor';
-                        return resolve(user);
-                    }
+                    await this.getDoctor(userId);
+                    user.role = 'doctor';
+                    return resolve(user);
                 } catch (e) {
                     console.log("Not a Doctor");
                 }
                 try {
-                    const testResult = await this.getPatient(userId);
-                    if (testResult) {
-                        user.testResult = testResult;
-                        user.role = 'patient';
-                        return resolve(user);
-                    }
+                    user.testResult = await this.getPatient(userId);
+                    user.role = 'patient';
+                    return resolve(user);
                 } catch (e) {
                     // A user but not found in any type tables.
                     console.log("Not a Patient");
                     return reject(e);
                 }
-            });
+            } catch (error) {
+                return reject(error);
+            }
         });
     }
 
     async getAuthority(userId: string): Promise<string> {
         const db: any = Container.get('mysql');
         const sql = 'SELECT privilege FROM Authority WHERE id = ?';
-        return new Promise((resolve, reject) => {
-            db.query(sql, userId, (error, result) => {
-                if (error || result?.length === 0) {
-                    return reject(error);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const [rows] = await db.query(sql, userId);
+                if (rows.length === 0) {
+                    return reject('Not an authority');
                 }
-                return resolve(result[0].privilege);
-            });
+                return resolve(rows[0].privilege);
+            } catch (error) {
+                return reject(error);
+            }
         });
     }
 
     async getPatient(userId: string): Promise<testResult> {
         const db: any = Container.get('mysql');
         const sql = 'SELECT testResult FROM Patient WHERE userId = ?';
-        return new Promise((resolve, reject) => {
-            db.query(sql, userId, (error, result) => {
-                return error ? reject(error) : resolve(result[0].testResult);
-            });
+        return new Promise(async (resolve, reject) => {
+            try {
+                const [rows] = await db.query(sql, userId);
+                if (rows.length === 0) {
+                    return reject('Not a patient');
+                }
+                return resolve(rows[0].testResult)
+            } catch (error) {
+                return reject(error);
+            }
         });
-
     }
 
-    async getDoctor(userId: string): Promise<boolean> {
+    async getDoctor(userId: string): Promise<void> {
         const db: any = Container.get('mysql');
         const sql = 'SELECT * FROM Doctor WHERE id = ?';
-        return new Promise((resolve, reject) => {
-            db.query(sql, userId, (error, result) => {
-                if (error || result?.length === 0) {
-                    return reject(error);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const [rows] = await db.query(sql, userId);
+                if (rows.length === 0) {
+                    return reject('Not a doctor');
                 }
-                return resolve(true);
-            });
+                return resolve();
+            } catch (error) {
+                return reject(error);
+            }
         });
     }
-
-
+    
 }
