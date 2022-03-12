d9aa1f41f366c1d4b92f0b4c5950f8bef661e688
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-03-12 11:06:02 -0500

    Update doctor-route.ts

--- src/api/routes/doctor-route.ts
+++ src/api/routes/doctor-route.ts
@@ -70,9 +70,11 @@ export default (app: Router) => {
 			{
 				params: Joi.object({
 					medicalId: Joi.string().required(),
-					licenseId: Joi.string().required()}),
+					licenseId: Joi.string().required()
+				}),
 				body: Joi.object({
-					checklist: Joi.array()})
+					checklist: Joi.array()
+				})
 			}),
 		async (req, res, next) => {
 			console.debug('Calling request symptoms from patient..');
@@ -99,10 +101,10 @@ export default (app: Router) => {
 			const doctorServiceInstance = Container.get(DoctorService);
 			doctorServiceInstance.getPatientSymptomsHistory(userId, req.params.licenseId as string,
 				req.params.medicalId as string).then((symptomsResponse) => {
-				return res.json(symptomsResponse);
-			}).catch((error) => {
-				return next(error);
-			});
+					return res.json(symptomsResponse);
+				}).catch((error) => {
+					return next(error);
+				});
 		}
 	);
 
@@ -139,6 +141,47 @@ export default (app: Router) => {
 			});
 		}
 	);
+
+	route.patch('/:medicalId/review', middleware.authenticateJWT,
+		celebrate({
+			params: Joi.object({
+				medicalId: Joi.string().required()
+			}),
+			body: Joi.object({
+				role: Joi.string().required()
+			})
+		}), async (req, res, next) => {
+			console.debug('Calling reviewed patient..');
+			const userId = getUserAuth(req.headers).user_id;
+			const doctorServiceInstance = Container.get(DoctorService);
+			doctorServiceInstance.reviewPatient(userId, req.params.medicalId).then((patient) => {
+				return res.status(200).end();
+			}).catch((error) => {
+				return next(error);
+			});
+		}
+	);
+
+	route.patch('/:medicalId/unreview', middleware.authenticateJWT,
+		celebrate({
+			params: Joi.object({
+				medicalId: Joi.string().required()
+			}),
+			body: Joi.object({
+				role: Joi.string().required()
+			})
+		}), async (req, res, next) => {
+			console.debug('Calling unreviewed patient..');
+			const userId = getUserAuth(req.headers).user_id;
+			const doctorServiceInstance = Container.get(DoctorService);
+			doctorServiceInstance.unReviewPatient(userId, req.params.medicalId).then((patient) => {
+				return res.status(200).end();
+			}).catch((error) => {
+				return next(error);
+			});
+		}
+	);
+
 
 };
 
