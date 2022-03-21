245f59165ceb42246cac0f638660871257f37444
Rayan Moarkech <rayanzone@hotmail.com>
2022-03-21 17:10:33 -0400

    Added emergencyLeave to return /doctors/all

--- src/services/doctor-service.ts
+++ src/services/doctor-service.ts
@@ -21,6 +21,7 @@ export default class DoctorService {
 							phoneNumber,
 							address,
 							email,
+       						emergencyLeave,
 							AssignedPatients.assignedPatientsCount
 					 FROM User,
 						  Doctor,
