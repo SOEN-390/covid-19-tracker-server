c05be1420b45fb73e440d602d0f8d30aed76a135
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-03-30 23:19:07 -0400

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,19 +4,6 @@ export default () => {
 
 	const mysql = require('promise-mysql2');
 
-	console.log('env');
-	
-	console.log(process.env);
-
-	console.log('database config:');
-
-	console.log({
-		host            : process.env.RDS_HOSTNAME,
-		user            : process.env.RDS_USERNAME,
-		password        : process.env.RDS_PASSWORD,
-		database        : process.env.RDS_PORT
-	});
-
 	const pool = mysql.createPool({
 		host: process.env.RDS_HOSTNAME,
 		user: process.env.RDS_USERNAME,
