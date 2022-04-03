fd8032ab0e45d57c782c756847aa31fe6ad64c50
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-04-02 23:13:57 -0400

    setup email server

---
+++ src/config/email.ts
@@ -0,0 +1,21 @@
+import nodemailer from 'nodemailer'
+export default () => {
+
+	const transport = nodemailer.createTransport({
+		host: "smtp.mailtrap.io",
+		port: 2525,
+		auth: {
+			user: "aa1c77d4befacd",
+			pass: "2eb3c4424aa957"
+		}
+	})
+
+
+	transport.sendMail({
+		from: 'quaranteam.soen390@gmail.com',
+		to: 'beshoysoliman11@gmail.com',
+		subject: 'Appoinment has been set with your Doctor',
+		html: `<p> Please login covid-19 tracker app and check appoiment set with your docter </p>
+			`
+	})
+};
